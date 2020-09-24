f2de667e94f989421cfe685e7051744c0551b0d8
McClane Howland <mcclane.howland@gmail.com>
2020-09-24 13:10:02 -0700

    coronavirus 2020_09_24__13_10_02

--- calpolycoronavirusdashboard/scrape.sh
+++ calpolycoronavirusdashboard/scrape.sh
@@ -2,3 +2,6 @@
 today=`date '+%Y_%m_%d__%H_%M_%S'`;
 filename="/Users/mcclane/Desktop/projects/webscraping/calpolycoronavirusdashboard/$today.html"
 curl "https://coronavirus.calpoly.edu/dashboard" --output $filename
+git add /Users/mcclane/Desktop/projects/webscraping/calpolycoronavirusdashboard
+git commit -m "coronavirus $today"
+git push

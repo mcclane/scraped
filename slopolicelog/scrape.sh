38259a4742557121d1a9fb70d01fc64f0dad7ec5
McClane Howland <mcclane.howland@gmail.com>
2020-09-25 18:15:50 -0700

    slopolicelog 2020_09_25__18_15_50

--- slopolicelog/scrape.sh
+++ slopolicelog/scrape.sh
@@ -3,5 +3,5 @@ today=`date '+%Y_%m_%d__%H_%M_%S'`;
 filename="/Users/mcclane/Desktop/projects/webscraping/slopolicelog/$today.html"
 curl "https://pdreport.slocity.org/policelog/rpcdsum.txt" --output $filename
 git add /Users/mcclane/Desktop/projects/webscraping/slopolicelog
-git commit -m "coronavirus $today"
+git commit -m "slopolicelog $today"
 git push
